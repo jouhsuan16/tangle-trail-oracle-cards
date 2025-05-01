7f9ad16402dd4c0f92fb7a7476a8469a43a9f0b8
jouhsuan16 <jouhsuan16@gmail.com>
2025-05-01 02:19:47 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -22,12 +22,12 @@ function draw() {
   background(255);
 
   if (currentCard) {
-    image(currentCard, width / 2, height / 2);
+    displayImageFit(currentCard);
     fill(0);
     textSize(16);
     text("Turn to the matching page in your oracle book.", width / 2, height - 40);
   } else {
-    image(cardBack, width / 2, height / 2);
+    displayImageFit(cardBack);
     fill(0);
     textSize(18);
     text("Click to draw your card", width / 2, height - 40);
@@ -36,4 +36,22 @@ function draw() {
 
 function mousePressed() {
   currentCard = random(cardImages);
+}
+
+// 自動縮放圖片以完整顯示
+function displayImageFit(img) {
+  let aspectCanvas = width / height;
+  let aspectImage = img.width / img.height;
+
+  let displayW, displayH;
+
+  if (aspectImage > aspectCanvas) {
+    displayW = width * 0.9;
+    displayH = displayW / aspectImage;
+  } else {
+    displayH = height * 0.75;
+    displayW = displayH * aspectImage;
+  }
+
+  image(img, width / 2, height / 2 - 20, displayW, displayH);
 }
